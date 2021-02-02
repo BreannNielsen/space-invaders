05498580eb47cfbb9c8a2e52b2a46f8fcf4645bf
BreannNielsen <63614249+BreannNielsen@users.noreply.github.com>
2021-02-02 09:48:27 -0800

    Added Bullet files

---
+++ Bullet/Bullet.gd
@@ -0,0 +1,13 @@
+extends KinematicBody2D
+
+var speed = 500
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_physics_process(true)
+
+func _physics_process(delta):
+	var collidedObject = move_and_collide(Vector2(0, -speed * delta))
+	if (collidedObject):
+		collidedObject.get_collider().queue_free() # queue_free requests that the object is "freed" aka deleted from the tree
+		queue_free() # this deletes the bullet
